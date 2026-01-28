d4c6f4ff549120cd963aa988bcc7ee9860279d56
wiliansconde <wilians.conde@gmail.com>
2026-01-28 03:02:09 -0300

    ajuste no root

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI(title="Calculadora API")
 @app.get("/")
 def root():
     return {
-        "message": "v9 API de Calculadora no ar",
+        "message": "v01 API de Calculadora no ar",
         "operations": ["soma", "subtracao", "multiplicacao", "divisao"]
     }
 
