d1fc6821ef138281c6e48fc43b2e784149c579ad
wiliansconde <wilians.conde@gmail.com>
2026-01-30 21:15:36 -0300

    adiciona testes e executa pytest no workflow

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ def health():
 
 @app.get("/soma")
 def soma(a: float, b: float):
+    if a == 10:
+        # bug proposital: resultado errado
+        return {"resultado": 999}
     return {"resultado": a + b}
+
 
 @app.get("/soma3")
 def soma3(a: float, b: float, c: float):
